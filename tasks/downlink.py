51a54ecb4129d0623e153620ce7b90b7dee24adc
Kelanu Ranganath <kelanucr@gmail.com>
2023-11-21 00:22:09 -0800

    Update downlink.py

--- tasks/downlink.py
+++ tasks/downlink.py
@@ -1,1 +1,23 @@
 #Task for sending data to the ground station
+#We're passed a state vector
+
+from Tasks.template_task import task
+
+class task(Task):
+    priority = 1
+    frequency = 1/10
+    name = 'downlink'
+    color = 'blue'
+
+    schedule_later = True
+
+
+    def senddata(self):
+        pass
+
+    async def main_task(self):
+        if self.satellite.isRecieving:
+            pass
+        else:
+            self.senddata(self.satellite.downlinkdata)
+        
