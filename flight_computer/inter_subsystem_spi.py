08f93d62071be47ac60dbcee700a1c0387dfa711
anniexiang01 <anniexiang01@gmail.com>
2024-04-02 10:36:20 -0700

    formatting wow

--- flight_computer/inter_subsystem_spi.py
+++ flight_computer/inter_subsystem_spi.py
@@ -6,11 +6,15 @@ from adafruit_bus_device.spi_device import SPIDevice
 
 import asyncio
 
-inter_subsystem_spi_bus = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO, slave_mode=False)
+inter_subsystem_spi_bus = busio.SPI(
+    board.SCK, MOSI=board.MOSI, MISO=board.MISO, slave_mode=False
+)
 
 test_board_cs = digitalio.DigitalInOut(board.D2)
 test_board_cs.direction = digitalio.Direction.OUTPUT
-test_board = SPIDevice(inter_subsystem_spi_bus, test_board_cs) # baudrate, polarity, etc. available here as kw params
+test_board = SPIDevice(
+    inter_subsystem_spi_bus, test_board_cs
+)  # baudrate, polarity, etc. available here as kw params
 
 
 async def send_receive(transmit_buffer, receive_buffer):
--- testing_board/code.py
+++ testing_board/code.py
@@ -5,15 +5,18 @@ sensorValue = 0x8080
 spiReadBytes = bytearray([0, 0])
 spiWriteBytes = bytearray([0, 0])
 
+
 async def spiWriteTask():
     global spiReadBytes, spiWriteBytes
     from inter_subsystem_spi import send_receive
+
     while True:
         # communicates commands with subsystem X
-        spiWriteBytes[0] = (sensorValue & 0xff00) >> 8
-        spiWriteBytes[1] = sensorValue & 0xff
+        spiWriteBytes[0] = (sensorValue & 0xFF00) >> 8
+        spiWriteBytes[1] = sensorValue & 0xFF
         await send_receive(spiWriteBytes, spiReadBytes)
         await asyncio.sleep(0.9)
+
 
 async def sensorReadTask():
     # regularly updates `sensorValue` based on the feedback from the sensor
@@ -21,6 +24,7 @@ async def sensorReadTask():
     while True:
         sensorValue += 3
         await asyncio.sleep(2.3)
+
 
 async def feedbackTask():
     # send debug data to the USB serial regularly
@@ -30,13 +34,16 @@ async def feedbackTask():
         print("Read", list(bytes(spiReadBytes)), "from SPI")
         await asyncio.sleep(1)
 
+
 async def idleTask():
     while True:
         # Forces CircuitPython to call `RUN_BACKGROUND_TASKS` at 10Hz to maintain USB connection
         await asyncio.sleep(0.02)
         await time.sleep(0.02)
 
+
 async def gatheredTask():
     await asyncio.gather(spiWriteTask(), sensorReadTask(), feedbackTask(), idleTask())
+
 
 asyncio.run(gatheredTask())
